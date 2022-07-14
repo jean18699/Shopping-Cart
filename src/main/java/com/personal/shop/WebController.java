d32754fed83143455b5600d48b6aefe6a8a325ee
jean <jean18699@gmail.com>
2022-07-14 06:55:23 -0400

    creating views

--- src/main/java/com/personal/shop/WebController.java
+++ src/main/java/com/personal/shop/WebController.java
@@ -1,9 +1,11 @@
 package com.personal.shop;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personal.shop.user.User;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class WebController {
@@ -13,6 +15,17 @@ public class WebController {
         model.addAttribute("title","Web Shopping");
 
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public void login(@ModelAttribute User user){
+        System.out.println(user.getUsername());
     }
 
 }
